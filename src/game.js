5ee3232d37a5d292bc428ef9e489d9b22a698712
Maks Melnyk <maks.melnik1611@gmail.com>
2025-03-08 23:17:26 +0200

    add all assets and upgrade lockey map

--- src/game.js
+++ src/game.js
@@ -16,11 +16,23 @@ let game = new Phaser.Game(config);
 
 function preload() {
     console.log("preload");
-    this.load.image('0', '../img/tile-000-socket.png');
-    this.load.image('1', '../img/tile-000-shadowed.png');
-    this.load.image('2', '../img/tile-000-shine.png');
-    this.load.image('L', '../img/tile-000-shine-L.png');
-    this.load.image('O', '../img/tile-000-shine-O.png');
+    this.load.audio('moveSound', '../assets/sfx/move.mp3');
+    this.load.image('0', '../assets/img/tile-000-socket.png');
+    this.load.image('1', '../assets/img/tile-000-shadowed.png');
+    this.load.image('2', '../assets/img/tile-002-active.png');
+    this.load.image('L', '../assets/img/tile-002-active-L.png');
+    this.load.image('O', '../assets/img/tile-002-active-O.png');
+    this.load.image('C', '../assets/img/tile-002-active-C.png');
+    this.load.image('K', '../assets/img/tile-002-active-K.png');
+    this.load.image('E', '../assets/img/tile-002-active-E.png');
+    this.load.image('Y', '../assets/img/tile-002-active-Y.png');
+    this.load.image('P', '../assets/img/tile-002-active-P.png');
+    this.load.image('W', '../assets/img/tile-002-active-W.png');
+    this.load.image('R', '../assets/img/tile-002-active-R.png');
+    this.load.image('down', '../assets/img/tile-002-button-down.png');
+    this.load.image('left', '../assets/img/tile-002-button-left.png');
+    this.load.image('right', '../assets/img/tile-002-button-right.png');
+    this.load.image('up', '../assets/img/tile-002-button-up.png');
 }
 
 function create() {
@@ -39,8 +51,11 @@ function create() {
         }
     }
 
+    let clickSound = this.sound.add('moveSound');
+
     this.input.on('pointerdown', (pointer) => {
         console.log("Pointer Down Event Fired!");
         console.log(`Clicked at: ${pointer.x}, ${pointer.y}`);
+        clickSound.play();
     });
 }
