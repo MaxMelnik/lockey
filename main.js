7105d538992158818e4c7003ead8bcfe578b073a
Maks Melnyk <maks.melnik1611@gmail.com>
2025-03-08 19:09:58 +0200

    make window borderless and dragable

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
 
 let mainWindow;
 
@@ -8,9 +9,22 @@ app.whenReady().then(() => {
         height: 600,
         frame: false,
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: false,
+            contextIsolation: true,
+            preload: path.join(__dirname, 'scripts/preload.js') // Load a secure script for communication
         }
     });
 
     mainWindow.loadFile('./pages/index.html');
+
+    // mainWindow.webContents.openDevTools();
+});
+
+// Handle window control messages
+ipcMain.on('close-app', () => {
+    app.quit();
+});
+
+ipcMain.on('minimize-app', () => {
+    mainWindow.minimize();
 });
